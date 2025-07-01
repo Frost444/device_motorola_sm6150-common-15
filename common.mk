35bb43140f0671208c83c43602cc7f33716814e9
Thiago <thiagojurema20@gmail.com>
2025-07-01 14:06:55 -0300

    fixup

--- common.mk
+++ common.mk
@@ -73,7 +73,7 @@ SPAMMY_LOG_TAGS := \
     sensors \
     vendor.qti.hardware.display.composer-service
 
-ifneq ($(TARGET_BUILD_VARIANT),eng)Add commentMore actions
+ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_VENDOR_PROPERTIES += \
     $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
 endif
